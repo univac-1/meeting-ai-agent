f0338b35f6162111d778500374c50f53e67b6544
rapirapi <bot0obok9hvu@gmail.com>
2025-01-20 00:53:39 +0900

    バックエンドとの疎通確認用にコンテンツ追加

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [backendData, setBackendData] = useState("");
 
   return (
     <>
@@ -18,18 +18,31 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {!backendData && (
+          <button
+            onClick={async () => {
+              const response = await fetch(
+                "https://meeting-ai-agent-132459894103.asia-northeast1.run.app/"
+              );
+              const data = await response.text();
+              setBackendData(data);
+            }}
+          >
+            Backendとの疎通確認
+          </button>
+        )}
+        {backendData && (
+          <div className="backend-data-success">
+            <h2>Backend Data Fetched Successfully!</h2>
+            <p>Response Data: {backendData}</p>
+          </div>
+        )}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
