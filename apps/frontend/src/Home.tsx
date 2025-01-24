48dac5d6cfe9c68ccf209377ae6db6df1cdd8d16
thistle0420 <thistle0420@gmail.com>
2025-01-24 20:18:16 +0900

    ホームページのタイトルを「会議管理システムだよ」に変更

--- apps/frontend/src/Home.tsx
+++ apps/frontend/src/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>会議管理システム</h1>
+      <h1>会議管理システムだよ</h1>
       <button onClick={() => navigate("/setup")}>会議を設定する</button>
       <button onClick={() => navigate("/auth")}>会議に参加する</button>
     </div>
