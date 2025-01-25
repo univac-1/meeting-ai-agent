283151d5c27af016cad5838a8cea18f58a64802e
rapirapi <bot0obok9hvu@gmail.com>
2025-01-25 23:07:06 +0900

    URL修正

--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -1,3 +1,4 @@
-const CLOUD_RUN_ENDPOINT = "http://localhost:8080"; // CloudRunのエンドポイント
+const CLOUD_RUN_ENDPOINT =
+  "https://meeting-ai-agent-132459894103.asia-northeast1.run.app"; // CloudRunのエンドポイント
 
 export { CLOUD_RUN_ENDPOINT };
